b506b5e7788d99a21a39a43fc61fe9d58dd1b6b1
JappaBR <guilherme.dion@gmail.com>
2021-05-12 07:27:34 -0300

    Adicionar arquivos de projeto.

---
+++ 21/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace _21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblPC = new System.Windows.Forms.Label();
+            this.lblJogador = new System.Windows.Forms.Label();
+            this.picAzul1 = new System.Windows.Forms.PictureBox();
+            this.picVerm2 = new System.Windows.Forms.PictureBox();
+            this.picVerm1 = new System.Windows.Forms.PictureBox();
+            this.picAzul2 = new System.Windows.Forms.PictureBox();
+            this.picAzul3 = new System.Windows.Forms.PictureBox();
+            this.picAzul4 = new System.Windows.Forms.PictureBox();
+            this.picAzul5 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picAzul1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picVerm2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picVerm1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAzul2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAzul3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAzul4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAzul5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.Location = new System.Drawing.Point(9, 12);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(119, 24);
+            this.Label1.TabIndex = 0;
+            this.Label1.Text = "Computador:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Jogador:";
+            // 
+            // lblPC
+            // 
+            this.lblPC.AutoSize = true;
+            this.lblPC.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPC.Location = new System.Drawing.Point(122, 12);
+            this.lblPC.Name = "lblPC";
+            this.lblPC.Size = new System.Drawing.Size(20, 24);
+            this.lblPC.TabIndex = 2;
+            this.lblPC.Text = "0";
+            // 
+            // lblJogador
+            // 
+            this.lblJogador.AutoSize = true;
+            this.lblJogador.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJogador.Location = new System.Drawing.Point(93, 240);
+            this.lblJogador.Name = "lblJogador";
+            this.lblJogador.Size = new System.Drawing.Size(20, 24);
+            this.lblJogador.TabIndex = 3;
+            this.lblJogador.Text = "0";
+            // 
+            // picAzul1
+            // 
+            this.picAzul1.Image = global::_21.Properties.Resources.baralhoA;
+            this.picAzul1.Location = new System.Drawing.Point(12, 282);
+            this.picAzul1.Name = "picAzul1";
+            this.picAzul1.Size = new System.Drawing.Size(119, 162);
+            this.picAzul1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picAzul1.TabIndex = 6;
+            this.picAzul1.TabStop = false;
+            // 
+            // picVerm2
+            // 
+            this.picVerm2.Image = global::_21.Properties.Resources.baralhoV;
+            this.picVerm2.Location = new System.Drawing.Point(155, 56);
+            this.picVerm2.Name = "picVerm2";
+            this.picVerm2.Size = new System.Drawing.Size(119, 162);
+            this.picVerm2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picVerm2.TabIndex = 5;
+            this.picVerm2.TabStop = false;
+            // 
+            // picVerm1
+            // 
+            this.picVerm1.Image = global::_21.Properties.Resources.baralhoV;
+            this.picVerm1.Location = new System.Drawing.Point(13, 56);
+            this.picVerm1.Name = "picVerm1";
+            this.picVerm1.Size = new System.Drawing.Size(119, 162);
+            this.picVerm1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picVerm1.TabIndex = 4;
+            this.picVerm1.TabStop = false;
+            // 
+            // picAzul2
+            // 
+            this.picAzul2.Image = global::_21.Properties.Resources.baralhoA;
+            this.picAzul2.Location = new System.Drawing.Point(155, 282);
+            this.picAzul2.Name = "picAzul2";
+            this.picAzul2.Size = new System.Drawing.Size(119, 162);
+            this.picAzul2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picAzul2.TabIndex = 7;
+            this.picAzul2.TabStop = false;
+            // 
+            // picAzul3
+            // 
+            this.picAzul3.Image = global::_21.Properties.Resources.baralhoA;
+            this.picAzul3.Location = new System.Drawing.Point(297, 282);
+            this.picAzul3.Name = "picAzul3";
+            this.picAzul3.Size = new System.Drawing.Size(119, 162);
+            this.picAzul3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picAzul3.TabIndex = 8;
+            this.picAzul3.TabStop = false;
+            this.picAzul3.Click += new System.EventHandler(this.picAzul3_Click);
+            // 
+            // picAzul4
+            // 
+            this.picAzul4.Image = global::_21.Properties.Resources.baralhoA;
+            this.picAzul4.Location = new System.Drawing.Point(448, 282);
+            this.picAzul4.Name = "picAzul4";
+            this.picAzul4.Size = new System.Drawing.Size(119, 162);
+            this.picAzul4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picAzul4.TabIndex = 9;
+            this.picAzul4.TabStop = false;
+            this.picAzul4.Visible = false;
+            this.picAzul4.Click += new System.EventHandler(this.picAzul4_Click);
+            // 
+            // picAzul5
+            // 
+            this.picAzul5.Image = global::_21.Properties.Resources.baralhoA;
+            this.picAzul5.Location = new System.Drawing.Point(597, 282);
+            this.picAzul5.Name = "picAzul5";
+            this.picAzul5.Size = new System.Drawing.Size(119, 162);
+            this.picAzul5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picAzul5.TabIndex = 10;
+            this.picAzul5.TabStop = false;
+            this.picAzul5.Visible = false;
+            this.picAzul5.Click += new System.EventHandler(this.picAzul5_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(142, 473);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 43);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Info;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(448, 473);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 43);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Finalizar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Info;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(297, 473);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 43);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Reiniciar";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(744, 541);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picAzul5);
+            this.Controls.Add(this.picAzul4);
+            this.Controls.Add(this.picAzul3);
+            this.Controls.Add(this.picAzul2);
+            this.Controls.Add(this.picAzul1);
+            this.Controls.Add(this.picVerm2);
+            this.Controls.Add(this.picVerm1);
+            this.Controls.Add(this.lblJogador);
+            this.Controls.Add(this.lblPC);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Jogo do 21";
+            ((System.ComponentModel.ISupportInitialize)(this.picAzul1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picVerm2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picVerm1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAzul2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAzul3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAzul4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAzul5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblPC;
+        private System.Windows.Forms.Label lblJogador;
+        private System.Windows.Forms.PictureBox picVerm1;
+        private System.Windows.Forms.PictureBox picVerm2;
+        private System.Windows.Forms.PictureBox picAzul1;
+        private System.Windows.Forms.PictureBox picAzul2;
+        private System.Windows.Forms.PictureBox picAzul3;
+        private System.Windows.Forms.PictureBox picAzul4;
+        private System.Windows.Forms.PictureBox picAzul5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ 21/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            picAzul3.Enabled = false;
+            button3.Enabled = false;
+        }
+        Random rnd = new Random();
+        int n1, n2, n3, n4, n5, n6, n7; //variaveis globais para as cartas.
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            n1 = rnd.Next(1, 11); //1º carta do jogador
+            n2 = rnd.Next(1, 11); //2º carta do jogador
+            n6 = rnd.Next(5, 11); //1º carta do pc
+            n7 = rnd.Next(5, 11); //2º carta do pc
+
+            picAzul1.Load("baralho1-" + n1 + ".png"); //1º carta do jogador
+            picAzul2.Load("baralho1-" + n2 + ".png"); //2º carta do jogador
+
+            lblPC.Text = "0";
+            int total = n1 + n2;
+            lblJogador.Text = total.ToString();
+
+            picAzul3.Load("baralhoA.png");
+            picAzul4.Load("baralhoA.png");
+            picAzul5.Load("baralhoA.png");
+
+            picAzul4.Visible = false;
+            picAzul5.Visible = false;
+
+            picAzul3.Enabled = true;
+            button1.Enabled = false;
+            button3.Enabled = true;
+        } // botão Iniciar.
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int pontosJogador = int.Parse(lblJogador.Text);
+            if (pontosJogador == 0)
+            {
+                Application.Exit();
+                return;
+            }
+            picVerm1.Load("baralho2-" + n6 + ".png");
+            picVerm2.Load("baralho2-" + n7 + ".png");
+            int pontosComputador = n6 + n7;
+            lblPC.Text = pontosComputador.ToString();
+            if (pontosJogador > pontosComputador)
+            {
+                MessageBox.Show("Parabéns pato, você venceu!");
+            }
+            else if (pontosComputador == pontosJogador)
+            {
+                MessageBox.Show("Empatamos!");
+            }
+            else
+            {
+                MessageBox.Show("Eita, você perdeu patinho!");
+                picAzul4.Enabled = false;
+
+            }
+            button2.Enabled = false;
+            picAzul3.Enabled = false;
+            picAzul4.Enabled = false;
+            picAzul5.Enabled = false;
+        } //botão finalizar.
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            lblJogador.Text = "Jogo Reiniciado";
+            lblPC.Text = "Jogo Reiniciado";
+            //carta do jogador
+            picAzul1.Load("baralhoA.png");
+            picAzul2.Load("baralhoA.png");
+            picAzul3.Load("baralhoA.png");
+
+            picAzul4.Visible = false;
+            picAzul4.Enabled = false;
+            picAzul5.Visible = false;
+            picAzul5.Enabled = false;
+
+            //carta do pc
+            picVerm1.Load("baralhoV.png");
+            picVerm2.Load("baralhoV.png");
+
+
+        }
+        private void picAzul3_Click(object sender, EventArgs e)
+        {
+            n3 = rnd.Next(1, 11);
+            int total = int.Parse(lblJogador.Text) + n3;
+            lblJogador.Text = total.ToString();
+            picAzul3.Load("baralho1-" + n3 + ".png");
+            if (total > 21)
+            {
+                MessageBox.Show("Eita! Você estourou, patinho!");
+                button2.Enabled = false;
+                picAzul4.Visible = false;
+                picAzul4.Enabled = false;
+
+            }
+            if (total == 21)
+            {
+                MessageBox.Show("Parabéns pato, você venceu!");
+            }
+            else
+            {
+                picAzul4.Visible = true;
+                picAzul4.Enabled = true;
+            }
+
+            picAzul3.Enabled = false;
+
+        } 
+        private void picAzul4_Click(object sender, EventArgs e)
+        {
+            n4 = rnd.Next(1, 11);
+            int total = int.Parse(lblJogador.Text) + n4;
+            lblJogador.Text = total.ToString();
+            picAzul4.Load("baralho1-" + n4 + ".png");
+            if (total > 21)
+            {
+                MessageBox.Show("Eita! Você estourou, patinho!");
+                button2.Enabled = false;
+                picAzul5.Visible = false;
+                picAzul5.Enabled = false;
+
+            }
+            else if (total == 21)
+            {
+                MessageBox.Show("Parabéns pato, você venceu!");
+            }
+            else
+            {
+                picAzul5.Enabled = true;
+                picAzul5.Visible = true;
+            }
+            picAzul4.Enabled = false;
+        } 
+        private void picAzul5_Click(object sender, EventArgs e)
+        {
+            n5 = rnd.Next(1, 11);
+            int total = int.Parse(lblJogador.Text) + n5;
+            lblJogador.Text = total.ToString();
+            picAzul5.Load("baralho1-" + n5 + ".png");
+            if (total > 21)
+            {
+                MessageBox.Show("Eita! Você estourou, patinho!");
+                button2.Enabled = false;
+            }
+            else if (total == 21)
+            {
+                MessageBox.Show("Parabéns pato, você venceu!");
+                button2.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Acabou as cartas, clique em finalizar!");
+            }
+            picAzul5.Enabled = false;
+        }
+    }
+}
